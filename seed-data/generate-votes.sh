518a99bd0c16858bcf303a833a8df9aaa7697df2
MATHANKAR <meghal.mathankar@capgemini.com>
2025-05-22 19:31:21 +0530

    updated the seed script

--- seed-data/generate-votes.sh
+++ seed-data/generate-votes.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # create 3000 votes (2000 for option a, 1000 for option b)
+# Update the URL to point to your server in VOTE_URL
+VOTE_URL="http://vote/"
 
-# Update the URL to point to your server
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" "$VOTE_URL"
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" "$VOTE_URL"
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" "$VOTE_URL"
